7b1abaa8f9d79dfca43c0e6dbddf9a82f45073dc
Denis Volin <denis.volin@mail.ru>
2022-10-17 23:44:56 +0300

    (nvim-tree) update configuration, remove deprecated settings

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -93,20 +93,13 @@ nvim_tree.setup {
     cmd = "trash",
     require_confirm = true,
   },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
-  },
   actions = {
     open_file = {
-        resize_window = true
+        resize_window = true,
+        quit_on_open = false,
+        window_picker = {
+          enable = true
+      },
     },
   },
 }
