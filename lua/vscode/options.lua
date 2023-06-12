a1a5ff14b2a303c58cb1481e40ed3a0cd2f8f95b
Denis Volin <denis.volin@mail.ru>
2023-06-12 22:39:11 +0300

    (vscode.options) smartcase search

--- lua/vscode/options.lua
+++ lua/vscode/options.lua
@@ -1,5 +1,7 @@
 local options = {
   clipboard = "unnamedplus",  -- allows neovim to access the system clipboard
+  ignorecase = true,          -- ignore case in search patterns
+  smartcase = true,           -- don't ingnore case if the search pattern contains upper case characters
 }
 
 for k, v in pairs(options) do
